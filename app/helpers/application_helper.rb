c1614c02cfdef280c75ca87ee27ac72b486ba603
ashes999 <ashes999@yahoo.com>
2014-12-24 21:17:10 +0000

    Added default title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+    
+  TITLE_SUFFIX = 'Momintum'
+  DEFAULT_TITLE = 'Mass Collaboration for the Ummah'
+  
+  # Returns the full title on a per-page basis.
+  def title_or_default(page_title = '') # turn nil into empty
+    if page_title.empty?
+      title = DEFAULT_TITLE
+    else
+      title = page_title
+    end
+    
+    return "#{title} | #{TITLE_SUFFIX}"
+  end
+    
 end
