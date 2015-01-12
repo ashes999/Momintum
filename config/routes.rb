6fd94a2dd855c81e0895b98612a0e6b3d27393e0
ashes999 <ashes999@yahoo.com>
2015-01-12 22:40:12 +0000

    Named routes for static pages: contact, updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root  'static_pages#home'
-  get   'static_pages/about'
-  get   'static_pages/contact'
-  get   'static_pages/updates'
+  get   'about'   => 'static_pages#about'
+  get   'contact' => 'static_pages#contact'
+  get   'updates' => 'static_pages#updates'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
