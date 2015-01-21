8bd177c0e0f5a0a85c69ed30b23b13554d04f2ad
ashes999 <ashes999@yahoo.com>
2015-01-21 23:02:29 +0000

    Consolidate two email test methods

--- app/mailers/momintum_mailer.rb
+++ app/mailers/momintum_mailer.rb
@@ -3,11 +3,18 @@ class MomintumMailer < Devise::Mailer
   helper :application # gives access to all helpers defined within `application_helper`.
   include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
   default template_path: 'mailers/devise' # changed from devise/mailers
+  default from: Devise.mailer_sender || 'robot@momintum.com'
   
   def send_test_email()
+    raise 'ADMIN_EMAIL environment variable not set' if ENV['ADMIN_EMAIL'].nil?
+    raise 'Devise.mailer_sender not set' if Devise.mailer_sender.nil?
+    
     # Create a user so we get the confirmation email; then delete him.
     u = User.create(:email => 'guest@guest.com')
     u.save!(:validate => false)
     u.delete
+    
+    # Also send a separate email to the admin
+    mail(:to => Devise.mailer_sender, :subject => 'Momintum test email')
   end
 end
--- lib/tasks/email.rake
+++ lib/tasks/email.rake
@@ -2,6 +2,6 @@ namespace :email do
   desc "Sends a test email using the Devise mailer"
   task test: :environment do
     MomintumMailer.send_test_email()
-    puts "Email sent to guest@guest.com (from #{Devise.mailer_sender}). Check the GMail outbox for confirmation."
+    puts "Emails sent to guest@guest.com (from #{Devise.mailer_sender}). Check the GMail outbox for confirmation."
   end
 end
