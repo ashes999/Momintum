6c6a87e6b3d3888c9e8263993886f8aecdb4e7ba
ashes999 <ashes999@yahoo.com>
2015-01-22 17:54:25 +0000

    Reliable email testing; delete unnecessary files.

--- app/mailers/momintum_mailer.rb
+++ app/mailers/momintum_mailer.rb
@@ -3,18 +3,19 @@ class MomintumMailer < Devise::Mailer
   helper :application # gives access to all helpers defined within `application_helper`.
   include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
   default template_path: 'mailers/devise' # changed from devise/mailers
-  default from: Devise.mailer_sender || 'robot@momintum.com'
+  default from: Devise.mailer_sender
   
   def send_test_email()
     raise 'ADMIN_EMAIL environment variable not set' if ENV['ADMIN_EMAIL'].nil?
+    raise 'TEST_ACCOUNT environment variable not set' if ENV['TEST_ACCOUNT'].nil?
     raise 'Devise.mailer_sender not set' if Devise.mailer_sender.nil?
     
     # Create a user so we get the confirmation email; then delete him.
-    u = User.create(:email => 'guest@guest.com')
+    u = User.create(:email => ENV['TEST_ACCOUNT'])
     u.save!(:validate => false)
     u.delete
     
-    # Also send a separate email to the admin
-    mail(:to => Devise.mailer_sender, :subject => 'Momintum test email')
+    # Send a separate email to the admin
+    mail(:to => ENV['ADMIN_EMAIL'], :subject => 'Momintum Test Email', :body => "This is a test email sent from #{root_url}").deliver
   end
 end
--- lib/tasks/email.rake
+++ lib/tasks/email.rake
@@ -1,7 +1,8 @@
 namespace :email do
   desc "Sends a test email using the Devise mailer"
   task test: :environment do
-    MomintumMailer.send_test_email()
-    puts "Emails sent to guest@guest.com (from #{Devise.mailer_sender}). Check the GMail outbox for confirmation."
+    result = MomintumMailer.send_test_email()
+    # Email is not sent until we evaluate #{result}
+    puts "Test email sent to #{ENV['ADMIN_EMAIL']} and registration to #{ENV['TEST_ACCOUNT']}. #{result}"
   end
 end
