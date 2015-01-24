ad0c4353824b8ef35349d55a6ca599eefaaba505
ashes999 <ashes999@yahoo.com>
2015-01-24 02:11:40 +0000

    Standardized feature_map access; dashboard shows features.

--- config/environment.rb
+++ config/environment.rb
@@ -21,8 +21,8 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-Rails.configuration.feature_map = FeatureMap.new
+Rails.application.config.feature_map = FeatureMap.new
 
 # Good place to change config (like mailer settings) since environment-specific
 # code is already evaluated. MomintumMailer.delivery_method doesn't do anything.
-ActionMailer::Base.delivery_method = :test if !Rails.configuration.feature_map.enabled?(:email)
+ActionMailer::Base.delivery_method = :test if !Rails.application.config.feature_map.enabled?(:email)
