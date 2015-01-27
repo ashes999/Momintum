551e8c09a7ad8e5331ecaee1bf4740d88c0cd98f
ashes999 <ashes999@yahoo.com>
2015-01-27 23:11:46 +0000

    Add user model tests (validation)

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(:email => 'test.user@test.com')
+  end
+  
+  test "users with an empty email are not valid" do
+    emails = [nil, '', '          ']
+    emails.each do |e|
+      @user.email = e
+      assert_not(@user.valid?)
+    end
+  end
 end
