72d9cef471f142756c154ccc9fab5059d3551694
ashes999 <ashes999@yahoo.com>
2015-02-20 22:36:29 +0000

    Like/dislike links are now ajax and DRY. \o/

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -16,7 +16,8 @@ if Rails.application.config.feature_map.enabled?(:like_sparks)
         Activity.create(:key => :likes_spark, :source_type => :user, :source_id => user_id, 
           :target_type => :spark, :target_id => spark_id) if Rails.application.config.feature_map.enabled?(:activity)
       end
-      #redirect_to Spark.find(spark_id)
+      
+      @spark = Spark.find(spark_id)
       
       respond_to do |format|
         format.html { redirect_to Spark.find(spark_id), :notice => 'You have liked this spark.' }
@@ -26,12 +27,15 @@ if Rails.application.config.feature_map.enabled?(:like_sparks)
     end
     
     def dislike
-      raise 'Missing user id' if params[:user_id].nil?
-      raise 'Missing spark id' if params[:spark_id].nil?
-      l = Like.find_by(:user_id => params[:user_id], :spark_id => params[:spark_id])
+      user_id = params[:user_id].to_i
+      spark_id = params[:spark_id].to_i
+      raise 'Missing user id' if user_id.nil?
+      raise 'Missing spark id' if spark_id.nil?
+      
+      l = Like.find_by(:user_id => user_id, :spark_id => spark_id)
       l.destroy unless l.nil?
       
-      #redirect_to Spark.find(params[:spark_id])
+      @spark = Spark.find(spark_id)
     end
   end
 end
