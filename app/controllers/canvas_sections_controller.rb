e1c5bd251efdf9aa0f887ec4118061cc17457d14
ashes999 <ashes999@yahoo.com>
2015-03-20 19:47:34 +0000

    Canvas section security + tests

--- app/controllers/canvas_sections_controller.rb
+++ app/controllers/canvas_sections_controller.rb
@@ -3,21 +3,23 @@ if Rails.application.config.feature_map.enabled?(:ideation)
     before_filter :authenticate_user!
     
     def create
-      # TODO: verify current user has edit permissions
-      
       spark = Spark.find(params[:spark_id])
-      # Place under the rest
-      lowest_canvas = spark.canvas_sections.order("y + height DESC").first
-      
-      if lowest_canvas.nil?
-        max_y = 0
+      if current_user.nil? || !current_user.can_edit?(spark)
+        message = 'You don\'t have permission to ideate this spark.' 
       else
-        max_y = lowest_canvas.y + lowest_canvas.height
+        # Place under the rest of the canvases
+        lowest_canvas = spark.canvas_sections.order("y + height DESC").first
+        
+        if lowest_canvas.nil?
+          max_y = 0
+        else
+          max_y = lowest_canvas.y + lowest_canvas.height
+        end
+        
+        section = CanvasSection.new({:spark_id => spark.id, :name => 'New Section', :x => 0, :y => max_y, :width => 300, :height => 150})
       end
       
-      section = CanvasSection.new({:spark_id => spark.id, :name => 'New Section', :x => 0, :y => max_y, :width => 300, :height => 150})
-      
-      if section.valid?
+      if !section.nil? && section.valid?
         section.save
         result = section
       else
@@ -26,26 +28,26 @@ if Rails.application.config.feature_map.enabled?(:ideation)
       end
       
       respond_to do |format|
-        format.html { puts "HTML"; redirect_to spark }
-        format.json { puts "JSON"; render json: result }
+        format.html { redirect_to section.nil? ? root_url : section.spark }
+        format.json { render json: result }
         format.js
       end
     end
     
     def update
-      # TODO: verify current user has edit permissions
-      
       sectionId = params[:sectionId] # eg. "section12"
       sectionId = sectionIdToId(sectionId)
+      message = 'Please specify the section' if sectionId.nil?
+      section = CanvasSection.find(sectionId)
+      message = 'You don\'t have permission to ideate this spark.' if current_user.nil? || !current_user.can_edit?(section.spark)
       
       x = params[:x]
       y = params[:y]
-      message = 'Please specify the section' if sectionId.nil?
+      
       message = 'Please specify the x position' if x.nil?
       message = 'Please specify the y position' if y.nil?
       
-      if message.nil?
-        section = CanvasSection.find(sectionId)
+      if message.nil? # valid input
         section.x = x
         section.y = y
         
@@ -60,24 +62,27 @@ if Rails.application.config.feature_map.enabled?(:ideation)
       result = { :error => message } if !message.nil?
       
       respond_to do |format|
-        format.html { puts "HTML"; redirect_to section.spark }
-        format.json { puts "JSON"; render json: result }
+        format.html { redirect_to section.nil? ? root_url : section.spark }
+        format.json { render json: result }
         format.js
       end
     end
     
     def destroy
-      puts "@@@ INSIDE, params are #{params}"
-      # TODO: verify current user has edit permissions
       sectionId = params[:sectionId] # eg. "section12"
       sectionId = sectionIdToId(sectionId)
       message = 'Please specify the section' if sectionId.nil?
       section = CanvasSection.find(sectionId)
-      section.destroy
+      
+      if current_user.nil? || !current_user.can_edit?(section.spark)
+        message = 'You don\'t have permission to ideate this spark.'
+      else
+        section.destroy
+      end
       
       respond_to do |format|
-        format.html { puts "HTML"; redirect_to section.spark }
-        format.json { puts "JSON"; render json: { success: true, message: message } }
+        format.html { redirect_to section.nil? ? root_url : section.spark }
+        format.json { render json: { success: true, message: message } }
         format.js
       end
     end
--- test/controllers/canvas_sections_controller_test.rb
+++ test/controllers/canvas_sections_controller_test.rb
@@ -1,51 +1,76 @@
-=begin
 require 'test_helper'
 
 class CanvasSectionsControllerTest < ActionController::TestCase
   setup do
-    @canvas_section = canvas_sections(:one)
+    @owner = create_user(:username => 'canvaseer')
+    @spark = create_spark(:name => 'Canvas Section Tests', :owner_id => @owner.id)
+    @canvas = CanvasSection.create(:spark_id => @spark.id, :name => 'Setup Canvas', :x => 0, :y => 0, :width => 100, :height => 100)
+    assert_not_nil(@canvas)
+    assert(@canvas.id > 0)
   end
-
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:canvas_sections)
+  
+  test 'can\'t create section without signing in as owner' do
+    base_count = @spark.canvas_sections.count
+    
+    post :create, {:spark_id => @spark.id }
+    @spark.reload
+    assert_equal(base_count, @spark.canvas_sections.count)
+    
+    hacker = create_user(:username => 'hacker37')
+    
+    sign_in(hacker)
+    post :create, {:spark_id => @spark.id }
+    @spark.reload
+    assert_equal(base_count, @spark.canvas_sections.count)
+    
+    sign_out(hacker)
+    sign_in(@owner)
+    post :create, {:spark_id => @spark.id }
+    @spark.reload
+    assert_equal(base_count + 1, @spark.canvas_sections.count)
   end
-
-  test "should get new" do
-    get :new
-    assert_response :success
+  
+  test 'can\'t edit section without signing in as owner' do
+    patch :update, {:sectionId => id_for(@canvas), :x => 99, :y => 99}
+    @canvas.reload
+    assert_equal(0, @canvas.x)
+    assert_equal(0, @canvas.y)
+    
+    hacker = create_user(:username => 'hacker37')
+    
+    sign_in(hacker)
+    patch :update, {:sectionId => id_for(@canvas), :x => 99, :y => 99}
+    @canvas.reload
+    assert_equal(0, @canvas.x)
+    assert_equal(0, @canvas.y)
+    
+    sign_out(hacker)
+    sign_in(@owner)
+    patch :update, {:sectionId => id_for(@canvas), :x => 99, :y => 99}
+    @canvas.reload
+    assert_equal(99, @canvas.x)
+    assert_equal(99, @canvas.y)
   end
-
-  test "should create canvas_section" do
-    assert_difference('CanvasSection.count') do
-      post :create, canvas_section: { height: @canvas_section.height, name: @canvas_section.name, spark_id: @canvas_section.spark_id, width: @canvas_section.width, x: @canvas_section.x, y: @canvas_section.y }
-    end
-
-    assert_redirected_to canvas_section_path(assigns(:canvas_section))
+  
+  test 'can\'t destroy section without signing in as owner' do
+    canvas = CanvasSection.create(:spark_id => @spark.id, :name => 'Destructo Canvas', :x => 0, :y => 0, :width => 200, :height => 100)
+    delete :destroy, {:sectionId => id_for(canvas)}
+    assert_not_nil(CanvasSection.find_by_id(canvas.id))
+    
+    hacker = create_user(:username => 'hacker37')
+    
+    sign_in(hacker)
+    delete :destroy, {:sectionId => id_for(canvas)}
+    assert_not_nil(CanvasSection.find_by_id(canvas.id))
+    
+    sign_out(hacker)
+    sign_in(@owner)
+    delete :destroy, {:sectionId => id_for(canvas)}
+    assert_nil(CanvasSection.find_by_id(canvas.id))
   end
-
-  test "should show canvas_section" do
-    get :show, id: @canvas_section
-    assert_response :success
+  
+  def id_for(section)
+    return "section#{section.id}"
   end
-
-  test "should get edit" do
-    get :edit, id: @canvas_section
-    assert_response :success
-  end
-
-  test "should update canvas_section" do
-    patch :update, id: @canvas_section, canvas_section: { height: @canvas_section.height, name: @canvas_section.name, spark_id: @canvas_section.spark_id, width: @canvas_section.width, x: @canvas_section.x, y: @canvas_section.y }
-    assert_redirected_to canvas_section_path(assigns(:canvas_section))
-  end
-
-  test "should destroy canvas_section" do
-    assert_difference('CanvasSection.count', -1) do
-      delete :destroy, id: @canvas_section
-    end
-
-    assert_redirected_to canvas_sections_path
-  end
+  
 end
-=end
