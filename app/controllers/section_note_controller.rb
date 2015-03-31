9e3f9b11249d3f3ae8eb08ab351e30cc3d78b30e
ashes999 <ashes999@yahoo.com>
2015-03-31 16:37:36 +0000

    Ideation: unit testing for delete note security

--- app/controllers/section_note_controller.rb
+++ app/controllers/section_note_controller.rb
@@ -1,23 +1,25 @@
-class SectionNoteController < ApplicationController
-  def update
-  end
+if Rails.application.config.feature_map.enabled?(:ideation)
+  class SectionNoteController < ApplicationController
+    before_filter :authenticate_user!
 
-  def destroy
-    note_id = params[:id] 
-    
-    message = 'Please specify the note' if note_id.nil?
-    note = SectionNote.find(note_id)
-    
-    if current_user.nil? || !current_user.can_edit?(note.canvas_section.spark)
-      message = 'You don\'t have permission to ideate this spark.'
-    else
-      note.destroy
+    def update
     end
-    
-    respond_to do |format|
-      format.html { redirect_to note.nil? ? root_url : note.canvas_section.spark }
-      format.json { render json: { success: true, message: message } }
-      format.js
+  
+    def destroy
+      note_id = params[:id] 
+      
+      note = SectionNote.find(note_id)
+      if current_user.nil? || !current_user.can_edit?(note.canvas_section.spark)
+        message = 'You don\'t have permission to ideate this spark.'
+      else
+        note.destroy
+      end
+      
+      respond_to do |format|
+        format.html { redirect_to note.nil? ? root_url : note.canvas_section.spark }
+        format.json { render json: { success: true, message: message } }
+        format.js
+      end
     end
   end
 end
